fae22458ed7771f14e113fd0f3f3f65e3f37a500
charlotte <charlotte>
2025-12-02 19:33:05 +0100

    json done

--- protocol.py
+++ protocol.py
@@ -1,3 +1,10 @@
+import json
 
 def make_message(action, **kwargs):
-    return {"action": action, **kwargs}
+    return json.dumps({
+        "action": action,
+        **kwargs
+    })
+
+def parse_message(raw):
+    return json.loads(raw)
